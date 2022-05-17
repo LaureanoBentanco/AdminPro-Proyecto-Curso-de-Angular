34b7ce2ffa53dd6fa2cda28ba50eb54aa6f8a0c7
usuario <laureano.bentancor@gmail.com>
2022-05-17 16:18:27 -0300

    se agrga la Funcionalidad Agregar Modificar y Eliminar

--- controlclientes/src/app/componentes/editar-clientes/editar-clientes.component.ts
+++ controlclientes/src/app/componentes/editar-clientes/editar-clientes.component.ts
@@ -1,4 +1,9 @@
+import { Cliente } from './../../modelo/cliente.model';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { ClientesServicio } from 'src/app/servicios/cliente.services';
+
 
 @Component({
   selector: 'app-editar-clientes',
@@ -7,9 +12,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarClientesComponent implements OnInit {
 
-  constructor() { }
+  clientes: Cliente [];
 
-  ngOnInit(): void {
+  cliente: Cliente = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    saldo: 0
+
+  }
+    id: string;
+  constructor(private clienteservicio: ClientesServicio,
+              private flashMessages: FlashMessagesService,
+              private router: Router,
+              private route: ActivatedRoute
+              ) { }
+
+  //codigo del Curso
+ /* ngOnInit() {
+
+    this.id = this.route.snapshot.params['id'];
+    this.clienteservicio.get_Cliente(this.id).subscribe( (cliente: Cliente) => {
+        this.cliente = cliente;
+      });
+}*/
+
+//codigo de prueba
+
+
+ngOnInit() {
+  this.id = this.route.snapshot.params['id'];
+  this.clienteservicio.get_Cliente(this.id).subscribe( (cliente: Cliente) => {
+  this.cliente = cliente;
+  })
+}
+guardar({value, valid}: {value: Cliente, valid: boolean}){
+    if(!valid){
+      this.flashMessages.show('Complete el Formulario Correctamente',
+                        {cssClass:'alert-danger',
+                        timeout: 4000});
+    }
+    else{
+      value.id = this.id;
+      //Modificar Cliente
+      this.clienteservicio.modificarCliente(value);
+      this.router.navigate(['']);
+
+    }
+}
+
+eliminar(){
+  if(confirm('Seguro que desea eliminar el cliente?')){
+    this.clienteservicio.eliminarCliente(this.cliente);
+    this.router.navigate(['/']);
+  }
+}
+
   }
 
-}
+
+
--- controlclientes/src/app/servicios/cliente.services.ts
+++ controlclientes/src/app/servicios/cliente.services.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@angular/core";
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from "@angular/fire/compat/firestore";
-import { Observable } from "rxjs";
-import { Cliente } from "../modelo/cliente.model";
+import { Observable, Subscribable } from "rxjs";
 import {map} from 'rxjs/operators';
+import { Cliente } from 'src/app/modelo/cliente.model';
 
 
 
@@ -13,7 +13,9 @@ export class ClientesServicio {
   clientesColleccion: AngularFirestoreCollection <Cliente>;
   clienteDoc!: AngularFirestoreDocument<Cliente>;
   clientes!: Observable<Cliente[]>;
-  cliente!: Observable<Cliente>;
+  cliente: Observable <Cliente> | any ;
+
+
 
   constructor (private db: AngularFirestore ){
     this.clientesColleccion = db.collection ('clientes', ref => ref.orderBy('nombre', 'asc'));
@@ -32,10 +34,56 @@ export class ClientesServicio {
     return this.clientes;
   }
     agregarcliente(cliente: Cliente){
-
       this.clientesColleccion.add(cliente);
-      
+    }
 
+
+    //codigo medificado del curso
+   /* get_Cliente(id: string): void{
+        this.clienteDoc = this.db.doc<Cliente>(`clientes/${id}`);
+        this.cliente = this.clienteDoc.snapshotChanges().pipe(
+          map( accion=>{
+              if(accion.payload.exists === false){
+                return null;
+              }
+
+              else{
+                const datos = accion.payload.data() as Cliente;
+                datos.id = accion.payload.id;
+                return datos;
+
+              }
+            })
+          );
+          return this.cliente;
+    }*/
+
+
+    get_Cliente(id: string){
+      this.clienteDoc = this.db.doc<Cliente>(`clientes/${id}`);
+      this.cliente = this.clienteDoc.snapshotChanges().pipe(
+          map( accion => {
+              if(accion.payload.exists === false){
+                  return null;
+              }
+              else{
+                  const datos = accion.payload.data() as Cliente;
+                  datos.id = accion.payload.id;
+                  return datos;
+              }
+          })
+      );
+      return this.cliente;
+  }
+    modificarCliente(cliente:Cliente){
+      this.clienteDoc = this.db.doc(`clientes/${cliente.id}`);
+      this.clienteDoc.update(cliente);
+    }
+
+    eliminarCliente(cliente:Cliente){
+
+    this.clienteDoc = this.db.doc(`clientes/${cliente.id}`);
+    this.clienteDoc.delete();
 
     }
 }
