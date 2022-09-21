ce4581476590c69fb9f95d30557b6fe03d4069d0
usuario <laureano.bentancor@gmail.com>
2022-09-21 13:17:41 -0300

    GUARDADO

--- Dad-Slow/dad-slow/src/app/app.component.spec.ts
+++ Dad-Slow/dad-slow/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-store'`, () => {
+  it(`should have as title 'dad-slow'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-store');
+    expect(app.title).toEqual('dad-slow');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-store app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('dad-slow app is running!');
   });
 });
